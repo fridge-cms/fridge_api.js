0c08f5422233df8fab1e75c7acb39c55629b8769
Mike Kruk <tamagokun@gmail.com>
2015-08-14 06:12:00 -0400

    0.0.9

--- dist/model.js
+++ dist/model.js
@@ -12,12 +12,46 @@ var Model = (function () {
     this.attrs = this.parse();
   }
 
-  Model.newFromPart = function newFromPart(part, data) {};
+  Model.newFromType = function newFromType(type) {
+    return new Model({
+      site_id: type.site_id,
+      document_definition_id: type.id,
+      content: type.parts.map(function (partDefinition) {
+        return { part_definition_id: part.id, name: part.name };
+      })
+    });
+  };
 
-  Model.prototype.commit = function commit() {};
+  Model.prototype.commit = function commit() {
+    var _this = this;
+
+    var _loop = function (key) {
+      var value = _this.raw[key];
+      if (Array.isArray(value)) {
+        if (_this._isPart(value)) {
+          value.forEach(function (part, i) {
+            var partName = _this._partName(part);
+            if (_this._partValue(partName) !== _this.attrs[partName]) {
+              _this.raw[key][i].value = _this.attrs[partName];
+            }
+          });
+        }
+      } else {
+        if (value !== _this.attrs[key]) {
+          _this.raw[key] = _this.attrs[key];
+        }
+      }
+    };
+
+    for (var key in this.raw) {
+      _loop(key);
+    }
+
+    return this.raw;
+  };
 
   Model.prototype.parse = function parse() {
-    var _this = this;
+    var _this2 = this;
 
     var hash = {};
     for (var key in this.raw) {
@@ -25,7 +59,11 @@ var Model = (function () {
       if (Array.isArray(value)) {
         if (this._isPart(value)) {
           value.forEach(function (part) {
-            hash[_this._partName(part)] = _this._partValue(part);
+            hash[_this2._partName(part)] = _this2._partValue(part);
+          });
+        } else if (this._isPartDefinition(value)) {
+          value.forEach(function (partDefinition) {
+            hash[_this2._partName(partDefinition)] = partDefinition;
           });
         } else {
           hash[key] = value.map(function (v) {
@@ -37,8 +75,8 @@ var Model = (function () {
       }
     }
 
-    var _loop = function (key) {
-      Object.defineProperty(_this, key, {
+    var _loop2 = function (key) {
+      Object.defineProperty(_this2, key, {
         get: function get() {
           return this.attrs[key] ? this.attrs[key] : undefined;
         },
@@ -49,7 +87,7 @@ var Model = (function () {
     };
 
     for (var key in hash) {
-      _loop(key);
+      _loop2(key);
     }
 
     return hash;
@@ -57,6 +95,10 @@ var Model = (function () {
 
   Model.prototype._isPart = function _isPart(val) {
     return val.length && val[0].part_definition_id;
+  };
+
+  Model.prototype._isPartDefinition = function _isPartDefinition(val) {
+    return val[0] && val[0].definition_id && val[0].definition_type;
   };
 
   Model.prototype._partName = function _partName(part) {
@@ -72,7 +114,3 @@ var Model = (function () {
 
 exports["default"] = Model;
 module.exports = exports["default"];
-
-// TODO
-
-// TODO
